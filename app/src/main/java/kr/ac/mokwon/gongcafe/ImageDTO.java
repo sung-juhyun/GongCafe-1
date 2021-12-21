4ff456a71250ae77ffe78036b28f0301b3d6ad78
13aeng <93965472+13aeng@users.noreply.github.com>
2021-12-21 16:30:09 +0900

    카페등록

---
+++ app/src/main/java/kr/ac/mokwon/gongcafe/ImageDTO.java
@@ -0,0 +1,11 @@
+package kr.ac.mokwon.gongcafe;
+
+public class ImageDTO {
+
+    public String imageUrl;
+    public String cafeName;
+    public String address;
+    public String info;
+    public String uid;
+    public String userId;
+}
